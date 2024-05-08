a99adc5c87ade2635c27e7dff7abd16ad3154ad5
Irving Canul <icanul@sol4it.net>
2024-05-08 11:21:57 -0600

    template

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(
+    private productsService: ProductsService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllSimple();
+  }
+
+  getAllSimple(){
+    this.productsService.getAllSimple()
+    .subscribe(products=>{
+      this.products = products;
+    });
+  }
+
+}
